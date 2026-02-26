31de28d0d4eebc218097fe0049423496b4c5c804
Abhinandan Chougule <askchougule@gmail.com>
2026-02-26 18:50:51 +0800

    Refactor SonarQube analysis script to simplify scanner call

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -2,15 +2,12 @@ def call(String scannerTool, String projectKey, String projectName) {
 
     def scannerHome = tool scannerTool
 
-    withSonarQubeEnv(scannerTool) {
-        withCredentials([string(credentialsId: 'SONAR_TOKEN', variable: 'SONAR_TOKEN')]) {
+    withSonarQubeEnv('Sonar') {
 
-            sh """
-                ${scannerHome}/bin/sonar-scanner \
-                  -Dsonar.projectKey=${projectKey} \
-                  -Dsonar.projectName=${projectName} \
-                  -Dsonar.login=\$SONAR_TOKEN
-            """
-        }
+        sh """
+            ${scannerHome}/bin/sonar-scanner \
+              -Dsonar.projectKey=${projectKey} \
+              -Dsonar.projectName=${projectName}
+        """
     }
 }
